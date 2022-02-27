0b76855435f45c42a2c6ba9b04494d9094dc70ff
nezhitsya <huyn9295@naver.com>
2022-02-27 20:07:52 +0900

    set up write

--- Book/Views/DetailViewController.swift
+++ Book/Views/DetailViewController.swift
@@ -14,6 +14,8 @@ class DetailViewController: UIViewController {
     private var inputTopPadding: NSLayoutConstraint?
     private var isInputContainerOpened: Bool = false
     
+    lazy private var writeTap = UITapGestureRecognizer(target: self, action: #selector(writeClicked))
+    
     private lazy var scrollView: UIScrollView = {
         let view = UIScrollView()
         view.isAccessibilityElement = false
@@ -22,6 +24,7 @@ class DetailViewController: UIViewController {
         view.addSubview(coverImage)
         view.addSubview(authorLabel)
         view.addSubview(descriptionLabel)
+        view.addSubview(writeButton)
         return view
     }()
     
@@ -53,6 +56,17 @@ class DetailViewController: UIViewController {
         view.numberOfLines = 0
         return view
     }()
+    
+    private lazy var writeButton: UIButton = {
+        let view = UIButton()
+        view.isAccessibilityElement = false
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.addGestureRecognizer(writeTap)
+        view.setTitle("한줄평 남기기", for: .normal)
+        view.titleLabel?.font = .boldSystemFont(ofSize: 15)
+        view.setTitleColor(UIColor(named: "colorGreen"), for: .normal)
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -72,6 +86,8 @@ class DetailViewController: UIViewController {
                 TextConverter.loadText(text: book.title) { bookTitle in
                     self?.titleLabel.attributedText = bookTitle
                     self?.titleLabel.font = .boldSystemFont(ofSize: 25)
+                    self?.titleLabel.textAlignment = .center
+                    self?.titleLabel.adjustsFontSizeToFitWidth = true
                 }
                 TextConverter.loadText(text: book.author) { authorText in
                     self?.authorLabel.attributedText = authorText
@@ -79,6 +95,8 @@ class DetailViewController: UIViewController {
                 }
                 TextConverter.loadText(text: book.description) { descText in
                     self?.descriptionLabel.attributedText = descText
+                    self?.descriptionLabel.font = UIFont.systemFont(ofSize: 15)
+                    self?.descriptionLabel.lineBreakMode = .byCharWrapping
                 }
             }
         }
@@ -98,23 +116,33 @@ class DetailViewController: UIViewController {
             
             titleLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 20),
             titleLabel.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 20),
+            titleLabel.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -20),
             
             authorLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
             authorLabel.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
 
             coverImage.topAnchor.constraint(equalTo: authorLabel.bottomAnchor, constant: 20),
             coverImage.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 10),
-            coverImage.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: 10),
+            coverImage.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -10),
             coverImage.heightAnchor.constraint(equalToConstant: 500),
             coverImage.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
             
             descriptionLabel.topAnchor.constraint(equalTo: coverImage.bottomAnchor, constant: 10),
+            descriptionLabel.bottomAnchor.constraint(equalTo: writeButton.topAnchor, constant: -30),
             descriptionLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 20),
-            descriptionLabel.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: 20),
-            descriptionLabel.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -20)
+            descriptionLabel.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -20),
+            
+            writeButton.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -20),
+            writeButton.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor)
         ]
         
         layouts.forEach { $0.isActive = true }
+    }
+    
+    @objc func writeClicked(_ sender: UITapGestureRecognizer) {
+        let vc = self.storyboard!.instantiateViewController(withIdentifier: "Write")
+        self.present(vc, animated: true, completion: nil)
     }
 
     /*
---
+++ Book/Views/WriteViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WriteViewController.swift
+//  Book
+//
+//  Created by 이다영 on 2022/02/27.
+//
+
+import UIKit
+
+class WriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
