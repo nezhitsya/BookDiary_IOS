8a0efff8473d88952ce1c457b5dcfbb15d1a030d
nezhitsya <huyn9295@naver.com>
2022-04-06 22:14:21 +0900

    set up write diary

--- Book/ViewModel/WriteViewModel.swift
+++ Book/ViewModel/WriteViewModel.swift
@@ -11,16 +11,18 @@ import Firebase
 class WriteViewModel: NSObject {
     
     let userUid = Auth.auth().currentUser?.uid
+    var title: String = ""
     var ref = Database.database().reference().child("Diary")
     
-    func write(title: String, image: String, desc: String, year: Int, month: Int, day: Int) {
+    func write(image: String, desc: String, year: Int, month: Int, day: Int, score: Int) {
         let diaryid: String = ref.childByAutoId().key!
-        ref.child(diaryid).child("title").setValue(title)
-        ref.child(diaryid).child("image").setValue(image)
-        ref.child(diaryid).child("desc").setValue(desc)
-        ref.child(diaryid).child("year").setValue(year)
-        ref.child(diaryid).child("month").setValue(month)
-        ref.child(diaryid).child("day").setValue(day)
+        ref.child(userUid!).child(diaryid).child("title").setValue(title)
+        ref.child(userUid!).child(diaryid).child("image").setValue(image)
+        ref.child(userUid!).child(diaryid).child("desc").setValue(desc)
+        ref.child(userUid!).child(diaryid).child("year").setValue(year)
+        ref.child(userUid!).child(diaryid).child("month").setValue(month)
+        ref.child(userUid!).child(diaryid).child("day").setValue(day)
+        ref.child(userUid!).child(diaryid).child("score").setValue(score)
     }
     
 }
--- Book/Views/WriteViewController.swift
+++ Book/Views/WriteViewController.swift
@@ -16,22 +16,32 @@ class WriteViewController: UIViewController {
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var coverImage: UIImageView!
     @IBOutlet var datePicker: UIDatePicker!
-    @IBOutlet var start1: UIImageView!
-    @IBOutlet var start2: UIImageView!
-    @IBOutlet var start3: UIImageView!
+    @IBOutlet var start1: UIButton!
+    @IBOutlet var start2: UIButton!
+    @IBOutlet var start3: UIButton!
     
     var yearData: Int = 0
     var monthData: Int = 0
     var dayData: Int = 0
-    var titleData: String = ""
     var imageData: String = ""
+    var starScore: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.descTextView.delegate = self
+        
+        self.descTextView.textColor = UIColor.lightGray
         self.yearData = UserDefaults.standard.value(forKey: "year") as! Int
         self.monthData = UserDefaults.standard.value(forKey: "month") as! Int
         self.dayData = UserDefaults.standard.value(forKey: "day") as! Int
+        
+        let calendar: NSCalendar = NSCalendar.current as NSCalendar
+        var components = calendar.components([NSCalendar.Unit.year, NSCalendar.Unit.month, NSCalendar.Unit.day], from: NSDate() as Date)
+        components.year = yearData
+        components.month = monthData
+        components.day = dayData
+        self.datePicker.setDate(calendar.date(from: components)!, animated: true)
         
         NotificationCenter.default.addObserver(self, selector: #selector(booknotificationReceived(_:)), name: .bookInfo, object: nil)
     }
@@ -40,6 +50,7 @@ class WriteViewController: UIViewController {
         guard let notificationUserInfo = notification.userInfo as? [String: String] else { return }
         
         TextConverter.loadText(text: notificationUserInfo["title"]!) { bookTitle in
+            self.viewModel.title = bookTitle!.string
             self.titleLabel.attributedText = bookTitle
             self.titleLabel.font = .boldSystemFont(ofSize: 25)
             self.titleLabel.textAlignment = .center
@@ -48,6 +59,7 @@ class WriteViewController: UIViewController {
         
         let data: Data = try! Data(contentsOf: URL(string: notificationUserInfo["image"]!)!)
         self.coverImage.image = UIImage(data: data)
+        self.imageData = notificationUserInfo["image"]!
     }
 
     /*
@@ -61,11 +73,60 @@ class WriteViewController: UIViewController {
     */
     
     @IBAction func saveClicked(_ sender: UIButton) {
-//        self.viewModel.write(title: <#T##String#>, image: <#T##String#>, desc: <#T##String#>, year: <#T##Int#>, month: <#T##Int#>, day: <#T##Int#>)
+        self.viewModel.write(image: imageData, desc: descTextView.text, year: yearData, month: monthData, day: dayData, score: starScore)
+        dismiss(animated: true, completion: nil)
     }
     
     @IBAction func cancelClicked(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
+    
+    @IBAction func didTapOne(_ sender: UIImageView) {
+        self.start1.setImage(UIImage(systemName: "star.fill"), for: .normal)
+        self.start1.tintColor = UIColor.orange
+        
+        self.start2.setImage(UIImage(systemName: "star"), for: .normal)
+        self.start2.tintColor = UIColor.secondaryLabel
+        
+        self.start3.setImage(UIImage(systemName: "star"), for: .normal)
+        self.start3.tintColor = UIColor.secondaryLabel
+        
+        self.starScore = 1
+    }
+    
+    @IBAction func didTapTwo(_ sender: UIImageView) {
+        self.start1.setImage(UIImage(systemName: "star.fill"), for: .normal)
+        self.start1.tintColor = UIColor.orange
+        
+        self.start2.setImage(UIImage(systemName: "star.fill"), for: .normal)
+        self.start2.tintColor = UIColor.orange
+        
+        self.start3.setImage(UIImage(systemName: "star"), for: .normal)
+        self.start3.tintColor = UIColor.secondaryLabel
+        
+        self.starScore = 2
+    }
+    
+    @IBAction func didTapThree(_ sender: UIImageView) {
+        self.start1.setImage(UIImage(systemName: "star.fill"), for: .normal)
+        self.start1.tintColor = UIColor.orange
+        
+        self.start2.setImage(UIImage(systemName: "star.fill"), for: .normal)
+        self.start2.tintColor = UIColor.orange
+        
+        self.start3.setImage(UIImage(systemName: "star.fill"), for: .normal)
+        self.start3.tintColor = UIColor.orange
+        
+        self.starScore = 3
+    }
 
+}
+
+extension WriteViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if(descTextView.textColor == UIColor.lightGray) {
+            descTextView.text = ""
+            descTextView.textColor = UIColor.black
+        }
+    }
 }
